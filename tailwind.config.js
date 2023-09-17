a9d4c6093d0d18c2328fadcba1f9f993e73c8f93
Richard Jonáš <jonas.richard11@gmail.com>
2023-09-17 21:29:46 +0200

    Font and adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        barlow:['Barlow Condensed', 'sans-serif;'],
+      }
+    },
   },
   plugins: [],
 }
